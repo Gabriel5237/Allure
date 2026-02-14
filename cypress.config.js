68f767ce7dfbcf25b20d657add0d412dab16300b
Gabriel Henrique <gabriel55237@gmail.com>
2026-02-13 23:27:44 -0300

    server teste 8080

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,22 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL || "http://localhost:3000",
-    
+    baseUrl: process.env.BASE_URL || "http://localhost:8080",
+
     setupNodeEvents(on, config) {
-      require("@shelex/cypress-allure-plugin/writer")(on, config);
+      allureWriter(on, config);
       return config;
-    }
-  }
+    },
+    env: {
+  allure: true
+}
+
+  },
 });
+
+
 
 
 
